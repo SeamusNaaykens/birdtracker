3b77df4d1854c8e8f79558fc7327d9a8917ed958
Seamus Naaykens <snaaykens@gmail.com>
2023-09-20 10:38:43 -0600

    Added an axios request to the Ebird API that fetches all bird groups for the bucketlist page

--- birdtracker/src/pages/Bucketlist/Bucketlist.js
+++ birdtracker/src/pages/Bucketlist/Bucketlist.js
@@ -1,9 +1,55 @@
 import './Bucketlist.scss'
 import { Collapse, Button, Box, useDisclosure } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Bucketlist() {
 
+    const [data, setData] = useState(null);
+    const [bird, setBird] = useState(null);
     const { isOpen, onToggle } = useDisclosure(false)
+    
+    const apiKeyEbird = process.env.EBIRD_API_KEY;
+    const apiKeyNuthatch = "d3faf1f9-0fc5-4ce1-9702-59a7f4d9662e"
+
+    useEffect(() => {
+        //  Replace with your actual API key
+        const apiUrl = process.env.EBIRD_API_URL; // Replace with the API endpoint you want to call
+    
+        axios
+          .get('https://api.ebird.org/v2/ref/sppgroup/merlin', {
+            headers: {
+                'x-ebirdapitoken': `Bearer ${apiKeyEbird}`,
+              'Content-Type': 'application/json',
+            },
+          })
+          .then((response) => {
+            setData(response.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          });
+      }, []);
+
+      useEffect(() => { 
+        axios
+        .get('https://nuthatch.lastelm.software/v2/birds', {
+          headers: {
+              'api-key': `Bearer ${apiKeyNuthatch}`,
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          setBird(response.data);
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+
+    }, []);
+
+   
 
     return (
         <main className='bucketlist'>
@@ -12,7 +58,30 @@ function Bucketlist() {
                 <p className='bucketlist__description'>Use this tool to learn about and keep track of your chase birds. Select a bird family and drag and drop the species to add them to your list. </p>
             </div>
             <div className='bucketlist__selector-container'>
-                <Button
+
+                {data && data.map((family) => {
+                    return (
+                       
+                        <Box>
+                        <Button
+                            onClick={onToggle}
+                            p='.75rem' border='none' background='#3a5a40' borderRadius='20px' margin='1rem' color='#dad7cd' fontFamily='Copy'>{family.groupName}
+                        </Button>
+                        <Collapse in={isOpen} animateOpacity>
+                <Box fontFamily='Copy' padding='1rem' background='#3a5a40' color='#dad7cd' border='1px solid #dad7cd ' width='13%' borderRadius='10px'>
+                    {/* {bird && bird.map((bird) => {
+                        return (
+                            <p className='bucketlist__bird'>{bird.name}</p>
+                        )
+                    }, [])}  */}
+                </Box>
+                </Collapse>
+                        </Box>
+                      
+                    )
+                }, [])}
+
+                {/* <Button
                     onClick={onToggle}
                     p='.75rem' border='none' background='#3a5a40' borderRadius='20px' margin='1rem' color='#dad7cd' fontFamily='Copy'>Waterfowl
             </Button>
@@ -28,7 +97,7 @@ function Bucketlist() {
                     <p className='bucketlist__bird'>Bufflehead</p>
                     <p className='bucketlist__bird'>Common Loon</p>
                 </Box>
-                </Collapse>
+                </Collapse> */}
             </div>
             <div className='bucketlist__container'>
 
